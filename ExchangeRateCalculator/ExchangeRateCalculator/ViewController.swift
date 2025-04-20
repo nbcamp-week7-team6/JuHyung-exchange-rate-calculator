9c8032ccc7d81b37d648b3dc26a4f1c1ee2d26d2
YunJuHyung <wngud5289@naver.com>
2025-04-21 04:15:01 +0900

    Lv2 compelete

--- ExchangeRateCalculator/ExchangeRateCalculator/ViewController.swift
+++ ExchangeRateCalculator/ExchangeRateCalculator/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
 
     private var currencyRates: [CurrencyRate] = []
     private let apiService = APIService()
+    private let searchBar = UISearchBar()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        configureTableView()
+        configureUI()
         fetchCurrentRateData()
 
     }
@@ -43,17 +45,33 @@ class ViewController: UIViewController {
         apiService.fetchData(url: url) { [weak self] (result: ModelData?) in
             guard let self else { return }
 
-            
+
             guard let result else {
-                   DispatchQueue.main.async {
-                       self.showAlert(title: "오류", message: "데이터를 불러올 수 없습니다.")
-                   }
-                   return
-               }
+                DispatchQueue.main.async {
+                    self.showAlert(title: "오류", message: "데이터를 불러올 수 없습니다.")
+                }
+                return
+            }
 
             let mappingData = result.rates.map{ (key, value) in
-                CurrencyRate(countryCode: key, rate: value)}
-            self.currencyRates = mappingData.sorted(by: {$0.countryCode < $1.countryCode})
+                let countryName: String
+                if let name = CountryMapping[key] {
+                    countryName = name
+                } else {
+                    print("일치하는 도시 코드 없음")
+                    countryName = "일치하는 도시 코드 없음"
+                }
+                return CurrencyRate(currencyCode: key, country: countryName, rate: value)
+            }
+            self.currencyRates = mappingData.sorted(by: {$0.currencyCode < $1.currencyCode})
+
+            //  MARK: -- 문제 발견 guard let 을 쓰니 map과정에서 return nil 발생
+//            let mappingData = result.rates.map{ (key, value) in
+//                guard let countryName = CountryMapping[key] else {
+//                    return
+//                }
+//                CurrencyRate(currencyCode: key, country: countryName, rate: value)}
+//            self.currencyRates = mappingData.sorted(by: {$0.currencyCode < $1.currencyCode})
             DispatchQueue.main.async(){
                 self.tableView.reloadData()
             }
@@ -68,20 +86,26 @@ class ViewController: UIViewController {
         self.present(alert, animated: true)
     }
 
-    private func configureTableView() {
+    private func configureUI() {
         view.addSubview(tableView)
+        view.addSubview(searchBar)
 
         tableView.snp.makeConstraints{
-            $0.top.bottom.leading.trailing.equalToSuperview()
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
 
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
     }
 }
 
 extension ViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        50
+        60
     }
 
 }
@@ -98,7 +122,7 @@ extension ViewController: UITableViewDataSource {
         }
 
         let item = currencyRates[indexPath.row]
-        cell.configure(with: item)
+        cell.configure(currencyCode: item.currencyCode, rate: item.rate)
         return cell
     }
 }
